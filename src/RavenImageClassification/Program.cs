2b4d544e7ee5e66ae11bb3d2b757c840d06ec254
Dejan Milicic <dejan.milicic@gmail.com>
2023-06-05 22:38:08 +0200

    restsharp

--- src/RavenImageClassification/Program.cs
+++ src/RavenImageClassification/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using RestSharp;
+
 Console.WriteLine("Hello, World!");
+
+var client = new RestClient(new RestClientOptions("http://localhost/"));
+
+var request = new RestRequest("api/predict", Method.Post);
+request.AddFile(
+        "image", 
+        @"C:\GitHub\imagercg-waiter\backend\test2.jpeg",
+        "image/jpeg");
+
+var response = client.Execute(request);
+
+var x= 1;

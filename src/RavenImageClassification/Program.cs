b9d67038772eb25b882e90943e5033ffaf9b6f7d
Dejan Milicic <dejan.milicic@gmail.com>
2023-06-05 22:57:51 +0200

    reading from the stream

--- src/RavenImageClassification/Program.cs
+++ src/RavenImageClassification/Program.cs
@@ -5,10 +5,10 @@ Console.WriteLine("Hello, World!");
 var client = new RestClient(new RestClientOptions("http://localhost/"));
 
 var request = new RestRequest("api/predict", Method.Post);
-request.AddFile(
-        "image", 
-        @"C:\GitHub\imagercg-waiter\backend\test2.jpeg",
-        "image/jpeg");
+
+using FileStream fs = File.OpenRead(@"C:\GitHub\imagercg-waiter\backend\test2.jpeg");
+
+request.AddFile(name: "image", getFile: ()=>fs, "image/jpeg");
 
 var response = client.Execute(request);
 

c2fe424560ff44113651a299c02421e24e548232
Dejan Milicic <dejan.milicic@gmail.com>
2023-06-06 22:18:55 +0200

    cleanup

--- src/RavenImageClassification/Program.cs
+++ src/RavenImageClassification/Program.cs
@@ -1,29 +1,5 @@
-﻿using System.Security.Cryptography.X509Certificates;
-using Raven.Client.Documents;
-using RavenImageClassification;
-using RavenImageClassification.Indexes;
-using RestSharp;
+﻿using RavenImageClassification;
 
 Console.WriteLine("Hello, World!");
 
 Seed.Do();
-return;
-
-///
-
-var client = new RestClient(new RestClientOptions("http://localhost/"));
-
-var request = new RestRequest("api/predict", Method.Post);
-
-using FileStream fs = File.OpenRead(@"C:\GitHub\imagercg-waiter\backend\test2.jpeg");
-
-request.AddFile(name: "image", getFile: ()=>fs, "image/jpeg");
-
-var response = client.Post<ClassifierResponse>(request);
-
-var x= 1;
-
-////////////////////
-
-
-

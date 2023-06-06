1a4f17a1b86359517702eb28d9acc5b509fe1ec0
Dejan Milicic <dejan.milicic@gmail.com>
2023-06-06 17:33:36 +0200

    index started

---
+++ src/RavenImageClassification/Indexes/Scans_ByObject.cs
@@ -0,0 +1,25 @@
+﻿using Raven.Client.Documents.Indexes;
+using RavenImageClassification.Models;
+
+namespace RavenImageClassification.Indexes;
+
+public class Scans_ByObject : AbstractIndexCreationTask<Scan, Scans_ByObject.Entry>
+{
+    public class Entry
+    {
+        public string[] Term { get; set; }
+    }
+
+    public Scans_ByObject()
+    {
+        Map = employees => from employee in employees
+
+            let attachmentNames = AttachmentsFor(employee)
+
+            select new Entry
+            {
+                Term = attachmentNames.Select(x => x.Name).ToArray()
+            };
+    }
+}
+
